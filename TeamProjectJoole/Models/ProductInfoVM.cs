0b8abd634d0a54eb2d390896d739c4a7d69ac267
aaliamer608 <aaliamer608@gmail.com>
2022-04-21 10:24:32 -0400

    Added Models

---
+++ TeamProjectJoole/Models/ProductInfoVM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TeamProjectJoole.Models
+{
+    public class ProductInfoVM
+    {
+        public int ProductID { get; set; }
+
+        public string Product_Name { get; set; }
+
+    }
+}
